d57e9781f0da76c697a8f06dea2da2e0f65bcf82
Robert Benavides <robertbenavidesgomez@gmail.com>
2024-04-01 15:24:59 +0200

    Primera prueba de UI

---
+++ app/src/main/java/com/example/memorycrafters/utils/ConfigurationUtils.java
@@ -0,0 +1,43 @@
+package com.example.memorycrafters.utils;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import com.example.memorycrafters.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.ActionCodeSettings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+@SuppressLint("RestrictedApi")
+public final class ConfigurationUtils {
+    private ConfigurationUtils() {
+        throw new AssertionError("No instance for you!");
+    }
+    public static boolean isGoogleMisconfigured(@NonNull Context context) {
+        return AuthUI.UNCONFIGURED_CONFIG_VALUE.equals(
+                context.getString(R.string.default_web_client_id));
+    }
+    @NonNull
+    public static List<AuthUI.IdpConfig> getConfiguredProviders(@NonNull Context context) {
+        List<AuthUI.IdpConfig> providers = new ArrayList<>();
+
+        if (!isGoogleMisconfigured(context)) {
+            providers.add(new AuthUI.IdpConfig.GoogleBuilder().build());
+        }
+
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+                .setAndroidPackageName("com.example.memorycrafters", true, null)
+                .setHandleCodeInApp(true)
+                .setUrl("https://google.com")
+                .build();
+
+        providers.add(new AuthUI.IdpConfig.EmailBuilder()
+                .setAllowNewAccounts(true)
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings)
+                .build());
+
+        return providers;
+    }
+}
